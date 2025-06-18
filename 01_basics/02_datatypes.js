5cfec142f3a7011aa5c9290156e960e26c6dce5e
shreya1415 <95966276+shreya1415@users.noreply.github.com>
2025-06-18 09:47:32 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello") //we are using node js not browser
+
+// alert(3 +3)
+
+console.log(3 + 3)
+console.log("Hello")
+
+let name = "shreya" //string
+let age = 25 //number
+let isLoggedIn = false //boolean
+// null - standalone value
+// symbol - unique
+// object
+console.log(typeof age); //number
+console.log(typeof null); //object
+console.log(typeof undefined); // undefined
+
+
